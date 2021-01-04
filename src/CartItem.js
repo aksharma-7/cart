ec873e014bdb6d2660a6de5b7b7a3a2cb518b54c
aksharma-7 <52443790+aksharma-7@users.noreply.github.com>
2021-01-04 15:13:47 +0530

    Used props to pass data from parent to child

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,16 +1,6 @@
 import React from 'react';
 
 class CartItem extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            price: 999,
-            title: 'Mobile Phone',
-            qty: 1,
-            img: ''
-        }
-        // this.increaseQuantity = this.increaseQuantity.bind(this);
-    }
 
     increaseQuantity = () => {
     //    console.log('this.state', this.state) 
@@ -42,9 +32,11 @@ class CartItem extends React.Component {
     }
 
     render () {
-        const {price, title, qty} = this.state;
+        console.log('this.prop', this.props)
+        const {price, title, qty} = this.props.product;
         return (
             <div className="cart-item">
+                {this.props.jsx}
                 <div className="left-block">
                     <img style= { styles.image } />
                 </div>
